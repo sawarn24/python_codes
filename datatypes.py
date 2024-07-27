a6260ab04c8403487090282bd110caf76c69219a
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-07-27 16:38:02 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+a=6
+print(a)
+c=5
+print(a+c)
+b="ishan"
+print(b)
+d= True
+e= None
+print("type of a is",type(a))
+print("type of a is",type(b))
+print("type of a is",type(d))
+print("type of a is",type(e))

1aee22bce6790075beb83f7a7385d31adf337f18
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-07-31 00:33:59 +0530

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,16 @@
+a="4"
+b="9"
+c=int(a)#typecasting
+d=int(b)#typecasting
+print(a+b)  #results showing without typecasting
+print(c+d)  #results showing with typecasting
+# THIS IS CALLED EXPLICIT TYPECASTING THAT MEANS WHEN WE CONVERT THE DATATYPE
+e=5
+f=5.6
+print(e+f)
+# THIS IS CALLED IMPLICIT TYPECASTING THAT MEANS WHEN TYPECONVERSION DONE AUTOMETICALLY
+
+
+
+
+

49b9543021de20eca5148e208167e99c9d660d2e
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-07-30 23:51:06 +0530

    Add files via upload

---
+++ arithmetic_operators.py
@@ -0,0 +1,7 @@
+print(15+7) #addition
+print(15-7) #subtraction
+print(15*7) #multiplication
+print(15/7) #division
+print(15//7)#floor division(gives answer in integer)
+print(5**3) #exponential
+print(5%3) #modulus(gives remainder)

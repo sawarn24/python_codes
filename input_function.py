802fa605a4b68ad60e2bc203e59a91a0fe39e214
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-07-31 00:34:31 +0530

    Add files via upload

---
+++ input_function.py
@@ -0,0 +1,9 @@
+a=input("ENTER YOUR NAME : ")
+b=input("ENTER YOUR AGE : ")
+print(a,"IS",b,"YEARS OLD")
+
+c=input("ENTER 1st NUMBER : ")
+d=input("ENTER 2nd NUMBER : ")
+print(c+d)
+print(int(c)+int(d))
+

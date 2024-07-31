bdf587c4ddee3067977687b53122ebd2950fb8c9
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-07-31 22:44:40 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,18 @@
+name="ishan"
+title='sawarn' # this shows we can declare a string in single quotes as well as in double quotes
+print(name+ title)
+print("NAME :",name+title)
+xyz="""dfmdsvmgodrmtdoh,tg
+vfgthgfngnfy
+gfrgthyj
+dffhyk       
+fvtrhk
+vfhy"""  #multiline strings need to be under triple single quote or triple double quotes
+print(xyz)
+
+#indexing
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])

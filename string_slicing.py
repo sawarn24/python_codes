b70c442e32b30db28eeee73018621f2dfd187391
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-07-31 22:45:22 +0530

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,7 @@
+name=("ishan sawarn")
+print(name[0:5])#string slicing
+# in this it shows that string is printing from 0th index to 4th index (5th one is excluded)
+print(len(name)) #tells the length of string
+text=("ishan")
+print(text[-4:-2])#negetive slicing
+print(text[len(text)-4:len(text)-2])

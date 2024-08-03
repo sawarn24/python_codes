3c85cca92881bdc3562526df94a884e55ff9ac93
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-08-03 22:42:04 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,13 @@
+a=int(input("ENTER YOUR AGE: "))
+print("YOUR AGE IS : ",a)
+if(a>=21):
+    print("YOU CAN DRINK AND DRIVE BUT NOT AT SAME TIME")
+elif(a>=18 and a<21):
+    print("YOU CAN DRIVE BUT NOT DRINK")
+else:
+    print("YOU ARE NOT AN ADULT")
+
+     
+    
+    
+    

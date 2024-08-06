7c55f8c505b82bd4456ad07cd8e84539003fca6d
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-08-06 22:53:10 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+name="ISHAN"
+for a in name:
+    print(a)
+colour=["red","green","blue","white"]
+for xyz in colour:
+    print(xyz)
+# for k in range(6):
+#     print(k)
+# for k in range(2,6):#2 is included but 6 is excluded
+#     print(k)
+for k in range(2,15,19):
+    print(k)

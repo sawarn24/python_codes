8ad4bb7572956d4a361e58a2ee262523514f8794
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-08-07 23:27:31 +0530

    Add files via upload

---
+++ break_continue.py
@@ -0,0 +1,12 @@
+for i in range(15):
+    print("5 * ",i+1,"=",5*(i+1))
+    if(i==9):
+        break
+for i in range(15):
+    if(i==5):
+     continue
+    print("8 * ",i+1,"=",8*(i+1))
+    
+        
+
+

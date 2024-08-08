64832167f42f776df3859026f9a2ad7bfe3ec169
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-08-08 23:06:44 +0530

    Add files via upload

--- for_loop.py
+++ for_loop.py
@@ -4,9 +4,9 @@ for a in name:
 colour=["red","green","blue","white"]
 for xyz in colour:
     print(xyz)
-# for k in range(6):
-#     print(k)
-# for k in range(2,6):#2 is included but 6 is excluded
-#     print(k)
-for k in range(2,15,19):
+for k in range(6):
+    print(k)
+for k in range(2,6):#2 is included but 6 is excluded
+    print(k)
+for k in range(2,15,3):# int this the third argument is called step it prints from 2 to 2+3....2+3+3...until it crosses 15
     print(k)

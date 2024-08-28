ee325de85f9c477cf213d16319a0d6bb85e454b2
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-08-28 23:25:07 +0530

    Add files via upload

---
+++ for_with_else.py
@@ -0,0 +1,17 @@
+for i in range(3):
+    print(i)
+else:
+    print("it's over")
+#let's create a empty list
+emp=[]
+for i in emp:
+    print(i)
+else:
+    print("over")
+for i in range(6):
+    print(i)
+    if(i== 3):
+        break
+else:
+    print("over")
+    

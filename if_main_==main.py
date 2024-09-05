a155fbe7e961e7b128c3b652237a69533af9745e
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-09-05 23:58:16 +0530

    Add files via upload

---
+++ if_main_==main.py
@@ -0,0 +1,9 @@
+# def intro():
+#     print(''' MY NAME IS ISHAN
+#           I AM 18 YEARS OLD
+#           .........''')
+# if __name=="__main__":
+   # intro()                                 this was written in sample_code
+import sample_code as s
+s.intro() # it will run two times..one time for function calling then the function calling in function call
+# for solving this we wrote line 5
